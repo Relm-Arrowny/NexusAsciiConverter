83b7c4172de16cd44c1df8584ef656f1a85d1fd0
Relm-Arrowny <ferimons@hotmail.com>
2023-07-07 14:12:59 +0100

    Corrected some typos

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # NexusAsciiConvertor
 Convert Nexus data to Ascii format
 
-All data with less than 1 value will go to the top of the ascii file and everything else will be in row and column.
+All data field contains one data point will go to the top of the ascii file and everything else will be in row and column (zero filled).
 
 To use see example folder.
--- example/exampleData.py
+++ example/exampleData.py
@@ -5,18 +5,23 @@ Created on 7 Jul 2023
 '''
 from src.nexusAsciiConvertor import nexusAsciiConvertor
 import os
-#for sigle files------------------------------------------------------------------------
+#for single files------------------------------------------------------------------------
 M = nexusAsciiConvertor()
-filename = 727909
+
+# input/output file name
+filename = 727909   
 inputFileName = "testData//i10-" +str(filename) +".nxs"
+
 outPutFileName = str(filename) + ".dat"
+
+#Do the conversion 
 M.convert(inputFileName, outPutFileName)
 
-#for the whole folder ---------------------------------------------------------------
+#Same as above but for the entire folder ---------------------------------------------------------------
 inPutFolder = "testData//"
 outputFolder = "testData//"
 for filename in sorted(os.listdir(inPutFolder)):
-    if filename[-4:] == ".nxs":
+    if filename[-4:] == ".nxs": #only work on .nxs extension 
         tempFilename = "%s%s.dat" %(outputFolder,filename[4:-4])  
         exist = os.path.isfile(tempFilename)
         M.convert(inPutFolder+filename, tempFilename)
