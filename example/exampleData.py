a86509f7c9b931989ec69b296856012a391c8d70
Relm-Arrowny <ferimons@hotmail.com>
2024-01-19 14:39:24 +0000

    data change

--- example/exampleData.py
+++ example/exampleData.py
@@ -4,10 +4,10 @@ Created on 7 Jul 2023
 @author: wvx67826
 '''
 from src.nexusAsciiConvertor import nexusAsciiConvertor
-import os
+import time, os, re
 #for single files------------------------------------------------------------------------
 M = nexusAsciiConvertor()
-
+"""
 # input/output file name
 filename = 727909   
 inputFileName = "testData//i10-" +str(filename) +".nxs"
@@ -17,11 +17,61 @@ outPutFileName = str(filename) + ".dat"
 #Do the conversion 
 M.convert(inputFileName, outPutFileName)
 
+"""
+"""
 #Same as above but for the entire folder ---------------------------------------------------------------
-inPutFolder = "testData//"
-outputFolder = "testData//"
+inPutFolder = "Z:\\2023\\mm32864-1\\"
+outputFolder = "Z:\\2023\\mm32864-1\\processing\\data\\"
 for filename in sorted(os.listdir(inPutFolder)):
     if filename[-4:] == ".nxs": #only work on .nxs extension 
         tempFilename = "%s%s.dat" %(outputFolder,filename[4:-4])  
         exist = os.path.isfile(tempFilename)
         M.convert(inPutFolder+filename, tempFilename)
+        
+"""
+# forever loop----------------
+
+inPutFolder = "C:\\Users\\wvx67826\\Desktop\\Data\\"#"Z:\\2023\\mm33256-1\\"
+outputFolder = "C:\\Users\\wvx67826\\Desktop\\Data\\"
+newScanNo = 786596
+lastScanNo = 786565
+timeOut = 0
+retryFlag = False
+while timeOut < 24*3600:
+    if retryFlag:
+        time.sleep(88)
+        inputFile = inPutFolder+"i10-"+ str(newScanNo) + ".nxs"
+        outputFile = outputFolder + "i10-" + str(newScanNo) + ".dat"
+        try:
+            M.convert(inputFile, outputFile)
+            retryFlag = False
+        except OSError:
+            print("scan running: %i" %newScanNo)
+            retryFlag = True
+        
+        
+    elif newScanNo == lastScanNo:
+        if retryFlag:
+            lastScanNo -= 1
+            retryFlag = False
+        print (lastScanNo)
+        time.sleep(88)
+        timeOut = timeOut+88
+        newScanNo = int(re.split("-|.nxs" ,sorted(os.listdir(inPutFolder))[-5])[1])
+       
+    else:
+        for filename in range(newScanNo,lastScanNo-1,-1):
+            inputFile = inPutFolder+"i10-"+ str(filename) + ".nxs"
+            outputFile = outputFolder + "i10-" + str(filename) + ".dat"
+            print(inputFile, outputFile)
+            try:
+                M.convert(inputFile, outputFile)
+            except FileNotFoundError:
+                print("no scan: %i" %filename)
+            except OSError:
+                print("scan running: %i" %filename)
+                retryFlag = True
+        lastScanNo = newScanNo
+        timeOut = 0
+
+        
